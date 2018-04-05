5323875c27f32fa6b0c09f72d8e43a891fcff700
Jeniffer Carvalho <jeniffer@princiweb.com.br>
2018-04-04 23:17:36 -0300

    adicionado sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,18 @@
  
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
  
 gulp.task('sass', function () {
   return gulp.src('./app/sass/style.scss')
+    .pipe(sourcemaps.init())
     .pipe(sass.sync().on('error', sass.logError))
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(gulp.dest('./app/css'));
 });
  
 gulp.task('sass:watch', function () {
-  gulp.watch('./app/sass/style.scss', ['sass']);
+  gulp.watch('./app/sass/**/*.scss', ['sass']);
 });
 
 gulp.task('default', ['sass:watch']);
